2378c029c123aead9dd5fdb5a6aefccb528ba13e
Yesid Balvin <33505875+yesidbalvin@users.noreply.github.com>
2020-04-21 09:01:24 -0500

    Add initial Files

---
+++ mqtt-iot-sf-consumer/Program.cs
@@ -0,0 +1,82 @@
+﻿using M2Mqtt;
+using M2Mqtt.Messages;
+using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+
+namespace mqtt_iot_sf_consumer
+{
+    class Program
+    {
+        private static ManualResetEvent manualResetEvent;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+            string iotEndpoint = "a3211ymll254ks-ats.iot.us-east-2.amazonaws.com";
+            int brokerPort = 8883;
+
+            Console.WriteLine("AWS IoT dotnet  message consumer starting ...");
+
+            var certFile = File.ReadAllBytes(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Amazon_Root_CA_1.crt"));
+            var certBase64 = Convert.ToBase64String(certFile);
+
+
+            var pfxFile = File.ReadAllBytes(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "certificate.cert.pfx"));
+
+            var base64 = Convert.ToBase64String(pfxFile);
+
+
+            var caCert = X509Certificate.CreateFromCertFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Amazon_Root_CA_1.crt"));
+            var clientCert = new X509Certificate2(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "certificate.cert.pfx"), "password");
+
+            var client = new MqttClient(iotEndpoint, brokerPort, true, caCert, clientCert, MqttSslProtocols.TLSv1_2);
+
+            client.MqttMsgSubscribed += IoTClient_MqttMsgSubscribed;
+            client.MqttMsgPublishReceived += IoTClient_MqttMsgPublishReceived;
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+            Console.WriteLine($"Connected to AWS IoT with client ID: {clientId}");
+
+            string topic = "sf/topic/+";//subscribe
+            Console.WriteLine($"Subscribing to topic: {topic}");
+            client.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+
+            //Keep the main thread alive for the event receivers to get invoked
+            KeepConsoleAppRunning(() =>
+            {
+                client.Disconnect();
+                Console.WriteLine("Disconnecting client...");
+            });
+        }
+
+        private static void IoTClient_MqttMsgPublishReceived(object sender, M2Mqtt.Messages.MqttMsgPublishEventArgs e)
+        {
+            Console.WriteLine("Message received: " + Encoding.UTF8.GetString(e.Message));
+        }
+
+        private static void IoTClient_MqttMsgSubscribed(object sender, M2Mqtt.Messages.MqttMsgSubscribedEventArgs e)
+        {
+            Console.WriteLine($"Successfully subscribed to the AWS IoT topic.");
+        }
+
+        private static void KeepConsoleAppRunning(Action onShutdown)
+        {
+            manualResetEvent = new ManualResetEvent(false);
+            Console.WriteLine("Press CTRL + C to exit");
+
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                onShutdown();
+                e.Cancel = true;
+                manualResetEvent.Set();
+            };
+
+            manualResetEvent.WaitOne();
+        }
+    }
+}
---
+++ mqtt-iot-sf-publisher/Program.cs
@@ -0,0 +1,43 @@
+﻿using M2Mqtt;
+using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+
+namespace mqtt_iot_sf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+            string iotEndpoint = "a3211ymll254ks-ats.iot.us-east-2.amazonaws.com";
+            Console.WriteLine("AWS IoT DotNet core message publisher starting");
+            int brokerPort = 8883;
+
+            string message = "Test message";
+            string topic = "sf/topic/1";//publish
+
+            var caCert = X509Certificate.CreateFromCertFile(Path.Join(AppContext.BaseDirectory, "Amazon_Root_CA_1.crt"));
+            var clientCert = new X509Certificate2(Path.Join(AppContext.BaseDirectory, "certificate.cert.pfx"), "password");
+
+            var client = new MqttClient(iotEndpoint, brokerPort, true, caCert, clientCert, MqttSslProtocols.TLSv1_2);
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+            Console.WriteLine($"Connected to AWS IoT with client id: {clientId}.");
+
+            int i = 0;
+            while (true)
+            {
+                client.Publish(topic, Encoding.UTF8.GetBytes($"{message} {i}"));
+                Console.WriteLine($"Published: {message} {i}");
+                i++;
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
